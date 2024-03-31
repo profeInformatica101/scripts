5807d7c17ee49031bb028052972fee3a48bd183e
Pablo <111131311+profeInformatica101@users.noreply.github.com>
2024-03-31 18:33:42 +0200

    Create actividad0.sh

---
+++ linux/actividad0.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+# ########## cambiar permisos con chmnod +x
+# COMENTARIOS
+echo ' A continuación salto de linea'
+echo -e
+echo 'Texto'
+echo 'Pulse enter para continuar...'
+read -n 0 -ers
+echo "Hola Mundo"
+echo 'Pulse enter para continuar..'
+read -n 0 -ers
